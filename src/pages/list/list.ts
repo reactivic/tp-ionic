17ab75a281334ced4fdd373328e10532d75449cd
sabativi <victor.sabatier@gmail.com>
2018-03-05 15:36:25 +0100

    add loader

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-list',
@@ -10,9 +11,23 @@ export class ListPage {
 
   private jobs;
 
-  constructor(public navCtrl: NavController,  public httpClient: HttpClient) {
+  constructor(public navCtrl: NavController,  public httpClient: HttpClient, public loadingCtrl: LoadingController) {
     this.jobs = [];
-    this.httpClient.get('https://mobile-api-jobs.herokuapp.com/api/jobs').subscribe(data => this.jobs = data);
+  }
+
+  ionViewDidLoad() {
+    this.fetchJobs();
+  }
+
+  fetchJobs() {
+    let loading = this.loadingCtrl.create({
+      content: "Veuillez patienter...",
+    });
+    loading.present();
+    this.httpClient.get('https://mobile-api-jobs.herokuapp.com/api/jobs').subscribe(data => {
+      loading.dismiss();
+      this.jobs = data
+    });
   }
 
 }
